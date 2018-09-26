6812173eda3003de4f4be4cbe32c4223834c2ab4
anandsuraj <surya13493@gmail.com>
2018-09-26 22:22:51 +0530

    disable zendesk

--- dashboard/index.php
+++ dashboard/index.php
@@ -42,13 +42,13 @@
       <meta name="theme-color" content="#ffffff">
       <script async src="https://www.googletagmanager.com/gtag/js?id=UA-118258744-1"></script> 
       <!--Start of Zendesk Chat Script-->
-            <script type="text/javascript">
+            <!-- <script type="text/javascript">
             window.$zopim||(function(d,s){var z=$zopim=function(c){z._.push(c)},$=z.s=
             d.createElement(s),e=d.getElementsByTagName(s)[0];z.set=function(o){z.set.
             _.push(o)};z._=[];z.set._=[];$.async=!0;$.setAttribute("charset","utf-8");
             $.src="https://v2.zopim.com/?3p9ply4ycWCZGB8npOhUB465VYOhRKfB";z.t=+new Date;$.
             type="text/javascript";e.parentNode.insertBefore($,e)})(document,"script");
-            </script>
+            </script> -->
 <!--End of Zendesk Chat Script-->
       <meta name="robots" content="index, follow">
    </head>
